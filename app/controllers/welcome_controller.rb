0da49ea86b2bc6e2015a08240c9b21330d4137f9
Vojtech Hyza & Karel Minarik <karmichucki@ataxointeractive.cz>
2012-07-10 15:35:41 +0200

    [FIX] Fixed incorrect `facet_filter` option and cleaned up the code
    * The `facet_filter` option for facets must be part of the facet definition,
      _not_ part of the _filter_ definition; see <http://www.elasticsearch.org/guide/reference/api/search/facets/index.html>
    
    * The correct syntax in Tire is `search.facet('myname', facet_filter: { terms: { foo: 'bar' } }) { terms 'myfield', size: 500 }`
    
    * Cleaned and tightened up the code
    
    Closes #1.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,74 +3,68 @@ class WelcomeController < ApplicationController
   def index
     paginate_per = 30
 
-    group_id = params[:part_group]
-    make_id = params[:make]
-    model_id = params[:model]
+    # Helper variables
+    # TODO: Remove, use just `params`?
+    #
+    group_id            = params[:part_group]
+    make_id             = params[:make]
+    model_id            = params[:model]
     vehicle_category_id = params[:category]
-    vehicle_power = params[:power]
-    capacity = params[:capacity]
-    q = params[:q]
+    vehicle_power       = params[:power]
+    vehicle_capacity    = params[:capacity]
+    q                   = params[:q]
 
     @results = Part.search do |search|
+
+      # 1. Search query
+      #
       search.query do |query|
         query.boolean do
+          # Return all results when no user query has been passed
+          # TODO: Use `sort` in this case?
+          #
           must { all } if q.blank?
+
+          # Search for user provided query
+          #
           must { string q, :default_operator => 'AND' } unless q.blank?
         end
       end
 
-      # filters
-      search.filter :term, 'vehicles.power' => vehicle_power unless vehicle_power.blank?
-      search.filter :term, 'vehicles.capacity' => capacity unless capacity.blank?
-
-      # facets
-      
-      # this is how I understand it should work - please uncomment lines 30-48
-      # 
-      if capacity.blank?
-        search.facet 'power' do
-          terms 'vehicles.power', :size => 1000, :all_terms => true
-        end
-      else
-        search.facet 'power' do
-          terms 'vehicles.power', :size => 1000, :all_terms => true, :facet_filter => {:term => {'vehicles.capacity' => capacity}}
-        end
-      end
-
-      if vehicle_power.blank?
-        search.facet 'capacity' do
-          terms 'vehicles.capacity', :size => 1000, :all_terms => true
-        end
-      else
-        search.facet 'capacity' do
-          terms 'vehicles.capacity', :size => 1000, :all_terms => true, :facet_filter => {:term => {'vehicles.power' => vehicle_power.to_i}}
-        end
-      end
-      # this below is working, but not filtering facet when one is selected
-      # comment this out (lines 52-58) when lines 30-48 are uncommented
+      # 2. Search filters
       #
-      # search.facet 'power' do
-      #   terms 'vehicles.power', :size => 1000, :all_terms => true
-      # end
+      vehicle_power_filter    = { 'vehicles.power'    => vehicle_power.to_i    }
+      vehicle_capacity_filter = { 'vehicles.capacity' => vehicle_capacity.to_i }
 
-      # search.facet 'capacity' do
-      #   terms 'vehicles.capacity', :size => 1000, :all_terms => true
-      # end
+      search.filter :term, vehicle_power_filter    unless vehicle_power.blank?
+      search.filter :term, vehicle_capacity_filter unless vehicle_capacity.blank?
 
-       # pagination
+      # 3. Facets
+      #
+      search.facet 'power',    ( vehicle_power.blank?    ? {} : { :facet_filter => { :term => vehicle_power_filter } } )    do
+        terms 'vehicles.power', :size => 1000, :all_terms => true
+      end
+
+      search.facet 'capacity', ( vehicle_capacity.blank? ? {} : { :facet_filter => { :term => vehicle_capacity_filter } } ) do
+        terms 'vehicles.capacity', :size => 1000, :all_terms => true
+      end
+
+      # Pagination
       search.from ((params[:page].present? ? params[:page].to_i : 1).to_i * paginate_per) - paginate_per
       search.size paginate_per
       
-      # debugging
+      # DEBUG
       @json_query = search.to_json
       @curl_query = search.to_curl
-      #raise search.to_json
-      #raise search.to_curl
+
+      Rails.logger.debug("JSON") { "\n" + @json_query + "\n" }
+      Rails.logger.debug("CURL") { "\n" + @curl_query + "\n" }
     end
 
     @categories = Category.includes(:makes).all
-    @makes = Make.includes(:models).all
-    @models = Model.all
-    @groups = PartGroup.all
+    @makes      = Make.includes(:models).all
+    @models     = Model.all
+    @groups     = PartGroup.all
   end
+
 end
